3b1ff65658d56607557f08b171ddbcce6b859ef7
lishoujun <lsjun@aliyun.com>
2019-10-07 21:51:11 +0800

    add a none app

--- shell/fedora.sh
+++ shell/fedora.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+sudo su -c "echo  'fastestmirror=true' >> /etc/dnf/dnf.conf"
+sudo su -c 'dnf copr enable librehat/shadowsocks -y'
 sudo dnf update -y
-sudo dnf install ffmpeg fish firefox git gnome-tweaks htop liveusb-creator qbittorrent vim vlc vsftpd -y
+sudo dnf install ffmpeg fish firefox git gnome-tweaks htop liveusb-creator qbittorrent vim vlc vsftpd shadowsocks-libev -y
