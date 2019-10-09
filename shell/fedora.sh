e00b2bb85aa82780c1d5ad619fdaad2ced6ae5f4
lishoujun <lsjun@aliyun.com>
2019-10-10 01:04:09 +0800

    update dnf first

--- shell/fedora.sh
+++ shell/fedora.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#sudo su -c "echo  'fastestmirror=true' >> /etc/dnf/dnf.conf"
-#sudo su -c "echo  'deltarpm=true' >> /etc/dnf/dnf.conf"
+date
+sudo su -c "echo  'fastestmirror=true' >> /etc/dnf/dnf.conf"
+sudo su -c "echo  'deltarpm=true' >> /etc/dnf/dnf.conf"
 
+dnf update dnf
 dnf copr enable librehat/shadowsocks -y
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
@@ -10,5 +12,6 @@ dnf update -y
 dnf install ffmpeg fish firefox git gnome-tweaks htop liveusb-creator qbittorrent vim vlc vsftpd shadowsocks-libev -y
 systemctl enable shadowsocks-libev-local.service
 
-pip install -U pip
-pip install -U urllib3
+pip3 install -U pip
+pip3 install -U urllib3
+date
