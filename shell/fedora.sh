de8307d50ec41bb27b4d852886bf656993051422
lishoujun <lsjun@aliyun.com>
2019-10-10 00:19:44 +0800

    update repo enable ss at start

--- shell/fedora.sh
+++ shell/fedora.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-sudo su -c "echo  'fastestmirror=true' >> /etc/dnf/dnf.conf"
-sudo su -c 'dnf copr enable librehat/shadowsocks -y'
-sudo dnf update -y
-sudo dnf install ffmpeg fish firefox git gnome-tweaks htop liveusb-creator qbittorrent vim vlc vsftpd shadowsocks-libev -y
+#sudo su -c "echo  'fastestmirror=true' >> /etc/dnf/dnf.conf"
+#sudo su -c "echo  'deltarpm=true' >> /etc/dnf/dnf.conf"
+
+dnf copr enable librehat/shadowsocks -y
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
+dnf update -y
+dnf install ffmpeg fish firefox git gnome-tweaks htop liveusb-creator qbittorrent vim vlc vsftpd shadowsocks-libev -y
+systemctl enable shadowsocks-libev-local.service
+
+pip install -U pip
+pip install -U urllib3
