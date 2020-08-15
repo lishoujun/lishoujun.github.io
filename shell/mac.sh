361f1551299d8afa46341ababc2a490a3ddbbb34
lishoujun <lishoujun@macdeMacBook-Pro-2.local>
2020-08-15 10:42:20 +0800

    add open connect

--- shell/mac.sh
+++ shell/mac.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # brew install some software.
+
+# update firest
+brew update
+## openconnect for vpn
+brew install openconnect
+# how to use: sudo openconnect vpn.xxx.com
 brew cask install quicksilver yed postman wechat miniconda brave-browser android-platform-tools
