30ac560b4bf0f12bc62a8d05ec95f76fe1bf4cff
lishoujun <highgogo@pm.me>
2020-09-07 16:50:59 +0800

    Update mac.sh
    add tuna help

--- shell/mac.sh
+++ shell/mac.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # brew install some software.
 
+# tuna mirror help
+# https://mirrors.tuna.tsinghua.edu.cn/help/homebrew/
 # update firest
 brew update
 ## openconnect for vpn
 brew install openconnect
 # how to use: sudo openconnect vpn.xxx.com
+git clone https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask --depth=1
 brew cask install quicksilver yed postman wechat miniconda brave-browser android-platform-tools
