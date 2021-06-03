b12551ff8cabe15ff76ea9a46926a9bf26ac96bd
lishoujun <highgogo@pm.me>
2021-06-04 00:15:02 +0800

    update thunderbird version

--- deepin/package/net.thunderbird/package.sh
+++ deepin/package/net.thunderbird/package.sh
@@ -1,19 +1,24 @@
 packageName=net.thunderbird
-version=78.10.2-20210513204504
+VERSION=78.11.0
 currentPath=`pwd`
-
 echo ${currentPath}
-rm -rf ${currentPath}/tmp/${packageName}
-# cd ../../../
-pwd
+
+rm -rf ${currentPath}/tmp/*
+
 mkdir -p tmp/${packageName}
 mkdir -p tmp/output
-cd tmp/${packageName}
+mkdir -p tmp/source
+cd tmp/source
+cp ~/Downloads/thunderbird-${VERSION}.tar.bz2 ./
+tar xvf thunderbird-${VERSION}.tar.bz2
+cd ${currentPath}/tmp/${packageName}
 
 mkdir -p opt/apps
 
 # 二进制
-cp -r /home/lishoujun/Documents/deb/thunderbird ./opt/apps/net.thunderbird
+# wget https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/${VERSION}/linux-x86_64/zh-CN/thunderbird-${VERSION}.tar.bz2
+
+cp -r ${currentPath}/tmp/source/thunderbird ./opt/apps/net.thunderbird
 # 包信息
 cp -r ${currentPath}/deepin/package/net.thunderbird/DEBIAN ./
 # 图标
@@ -22,4 +27,4 @@ cp -r ${currentPath}/deepin/package/net.thunderbird/Thunderbird.desktop ./usr/sh
 
 cd ${currentPath}/tmp
 du ${packageName}
-dpkg-deb -bv ${packageName} output/${packageName}_${version}_amd64.deb
+dpkg-deb -bv ${packageName} output/${packageName}_${VERSION}_amd64.deb
