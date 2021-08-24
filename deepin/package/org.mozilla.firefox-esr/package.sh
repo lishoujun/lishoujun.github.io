15d1f19e2d8fdfe801bd9b3aac721c3a6e07ee84
lishoujun <highgogo@pm.me>
2021-08-24 22:38:59 +0800

    优化代码结构

--- deepin/package/org.mozilla.firefox-esr/package.sh
+++ deepin/package/org.mozilla.firefox-esr/package.sh
@@ -1,5 +1,7 @@
 packageName=org.mozilla.firefox-esr
 VERSION=91.0.1esr
+URL=https://download-installer.cdn.mozilla.net/pub/firefox/releases/${VERSION}/linux-x86_64/zh-CN/firefox-${VERSION}.tar.bz2
+
 currentPath=`pwd`
 echo ${currentPath}
 
@@ -8,24 +10,25 @@ rm -rf ${currentPath}/tmp/*
 mkdir -p tmp/${packageName}
 mkdir -p tmp/output
 mkdir -p tmp/source
-
-
 cd ${currentPath}/tmp/source
-wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/${VERSION}/linux-x86_64/zh-CN/firefox-${VERSION}.tar.bz2
+wget ${URL}
 
 tar xvf firefox-${VERSION}.tar.bz2
-file_kb=$((`du --max-depth=0 firefox|awk '{print $1}'`))
-echo ${file_kb}
+installed_size=$((`du --max-depth=0 firefox|awk '{print $1}'`))
+echo ${installed_size}
 cd ${currentPath}/tmp/${packageName}
 
 mkdir -p opt/apps
 
-cp -r ${currentPath}/tmp/source/firefox ./opt/apps/org.mozilla.firefox-esr
+cp -r ${currentPath}/tmp/source/firefox ./opt/apps/${packageName}
 # 包信息
-cp -r ${currentPath}/deepin/package/org.mozilla.firefox-esr/DEBIAN ./
+cp -r ${currentPath}/deepin/package/${packageName}/DEBIAN ./
+sed -i "s/Installed-Size:.*/Installed-Size: ${installed_size}/" control
+sed -i "s/Version:.*/Version: ${VERSION}/" control
+
 # 图标
 mkdir -p usr/share/applications
-cp -r ${currentPath}/deepin/package/org.mozilla.firefox-esr/Firefox-esr.desktop ./usr/share/applications/
+cp -r ${currentPath}/deepin/package/${packageName}/Firefox-esr.desktop ./usr/share/applications/
 
 cd ${currentPath}/tmp
 du ${packageName}
