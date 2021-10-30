43a5d7834960d501eb2814b053657af9fc4219d7
lishoujun <highgogo@pm.me>
2021-10-30 21:29:32 +0800

    change name in thunderbird desktop

--- deepin/package/installformurl.sh
+++ deepin/package/installformurl.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -x
+cd /tmp
+mkdir -p debinstall
+cd debinstall
+rm -rf *.deb
 echo $0
 echo $1
-url=http://cdn.download.yyyit.com/output/net.thunderbird_91.2.1_amd64.deb
-rm tmp.deb
+url=http://cdn.download.yyyit.com/output/com.browser.softedge_95.0.1020.40-1_amd64.deb
 wget -O tmp.deb ${url}
 sudo dpkg -i tmp.deb
