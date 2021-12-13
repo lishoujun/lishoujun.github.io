0c672ac3318b5b2d8b18e32e416c2b8b65060ad8
lishoujun <highgogo@pm.me>
2021-12-13 15:56:06 +0800

    Update mac.sh
    换公司了 已经不用anyconnect了 偶尔用go

--- shell/mac.sh
+++ shell/mac.sh
@@ -5,8 +5,7 @@
 # https://mirrors.tuna.tsinghua.edu.cn/help/homebrew/
 # update firest
 brew update
-## openconnect for vpn
-brew install openconnect
-# how to use: sudo openconnect vpn.xxx.com
+
 git clone https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask --depth=1
-brew install quicksilver yed postman wechat miniconda brave-browser android-platform-tools
+brew install yed postman wechat miniconda android-platform-tools
+brew install go
