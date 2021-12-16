c8da9ccbda7a57a8da9b6c2e844e4fb6be570ea0
lishoujun <highgogo@pm.me>
2021-12-17 00:10:56 +0800

    Update mac.sh

--- shell/mac.sh
+++ shell/mac.sh
@@ -3,9 +3,13 @@
 
 # tuna mirror help
 # https://mirrors.tuna.tsinghua.edu.cn/help/homebrew/
-# update firest
+# update first
 brew update
 
 git clone https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask --depth=1
+# 常用软件  
 brew install yed postman wechat miniconda android-platform-tools
-brew install go
+# 安装 go 运行环境
+brew install go 
+# 安装greadlink
+brew install coreutils
