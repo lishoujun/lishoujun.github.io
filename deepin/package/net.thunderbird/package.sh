6a29905f68f0ecd65d2e9b1e8e3179957e1ba2e5
lsjun <highgogo@pm.me>
2023-06-03 14:53:56 +0000

    102.12
    Signed-off-by: lsjun <highgogo@pm.me>

--- deepin/package/net.thunderbird/package.sh
+++ deepin/package/net.thunderbird/package.sh
@@ -1,7 +1,7 @@
 set -x
 packageName=net.thunderbird
 shortname=thunderbird
-VERSION=102.11.0
+VERSION=102.11.2
 URL=https://download-installer.cdn.mozilla.net/pub/${shortname}/releases/${VERSION}/linux-x86_64/zh-CN/${shortname}-${VERSION}.tar.bz2
 
 currentPath=`pwd`
