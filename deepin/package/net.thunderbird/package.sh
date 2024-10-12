3939f1a87c3d266a7227f37166e8d6034265fc4a
lsjun <highgogo@pm.me>
2024-10-12 09:17:27 +0000

    upgrade fx and tb, specially fix fx issue
    Signed-off-by: lsjun <highgogo@pm.me>

--- deepin/package/net.thunderbird/package.sh
+++ deepin/package/net.thunderbird/package.sh
@@ -1,7 +1,7 @@
 set -x
 packageName=net.thunderbird
 shortname=thunderbird
-VERSION=115.15.0
+VERSION=115.16.0
 URL=https://download-installer.cdn.mozilla.net/pub/${shortname}/releases/${VERSION}/linux-x86_64/zh-CN/${shortname}-${VERSION}.tar.bz2
 
 currentPath=`pwd`
