f0e918f9668117de9e31c77aeac160c1f30904dd
highgogo <highgogo@pm.me>
2025-06-25 23:16:35 +0800

    Update VERSION to 128.12.0esr in package.sh for Firefox ESR

--- deepin/package/org.mozilla.firefox-esr/package.sh
+++ deepin/package/org.mozilla.firefox-esr/package.sh
@@ -1,7 +1,7 @@
 set -x
 packageName=org.mozilla.firefox-esr
 shortname=firefox
-VERSION=128.11.0esr
+VERSION=128.12.0esr
 URL=https://download-installer.cdn.mozilla.net/pub/${shortname}/releases/${VERSION}/linux-x86_64/zh-CN/${shortname}-${VERSION}.tar.bz2
 
 currentPath=`pwd`
