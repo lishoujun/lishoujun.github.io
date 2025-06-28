2d198b7b01c2cae6de70f0f60c8a695f12b43e69
highgogo <highgogo@pm.me>
2025-06-28 10:13:20 +0800

    Update VERSION to 140.0esr in package.sh for Firefox ESR

--- deepin/package/org.mozilla.firefox-esr/package.sh
+++ deepin/package/org.mozilla.firefox-esr/package.sh
@@ -2,7 +2,7 @@ set -x
 packageName=org.mozilla.firefox-esr
 shortname=firefox
 # VERSION=128.12.0esr
-VERSION=140.0.0esr
+VERSION=140.0esr
 
 URL=https://download-installer.cdn.mozilla.net/pub/${shortname}/releases/${VERSION}/linux-x86_64/zh-CN/${shortname}-${VERSION}.tar.bz2
 
